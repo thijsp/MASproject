dbb9f3de2910b05b407fa2f9069e5fe7e7a2eff6
Anthony Clays <anthony@clays.me>
2017-05-21 12:08:29 +0200

    Update SimGenerator
    - Changed PlaneRoadModel to NoFlyZoneRoadModel (and updated the
    RoadModel renderer)
    - Modified UAV creation to compute its starting position ahead of time
    - Changed the number of UAVs to one for testing purposes

--- src/main/java/simulation/SimGenerator.java
+++ src/main/java/simulation/SimGenerator.java
@@ -9,22 +9,20 @@ import agents.UAV;
 import com.github.rinde.rinsim.core.Simulator;
 import com.github.rinde.rinsim.core.model.comm.CommModel;
 import com.github.rinde.rinsim.core.model.pdp.DefaultPDPModel;
-import com.github.rinde.rinsim.core.model.road.PlaneRoadModel;
+import com.github.rinde.rinsim.core.model.road.NoFlyZoneRMB;
 import com.github.rinde.rinsim.core.model.road.RoadModel;
-import com.github.rinde.rinsim.core.model.road.RoadModelBuilders;
-import com.github.rinde.rinsim.core.model.road.RoadModelBuilders.PlaneRMB;
 import com.github.rinde.rinsim.geom.Point;
 import com.github.rinde.rinsim.ui.View;
 import com.github.rinde.rinsim.ui.View.Builder;
-import com.github.rinde.rinsim.ui.renderers.CommRenderer;
-import com.github.rinde.rinsim.ui.renderers.PlaneRoadModelRenderer;
+import com.github.rinde.rinsim.ui.renderers.NoFlyZoneRoadModelRenderer;
 import com.github.rinde.rinsim.ui.renderers.RoadUserRenderer;
+import geom.Rectangle;
 import org.apache.commons.math3.random.RandomGenerator;
 import org.eclipse.swt.graphics.RGB;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public final class SimGenerator {
     static final double MIN_SPEED = 10.0D;
@@ -36,12 +34,11 @@ public final class SimGenerator {
     static final Point MAX_POINT = new Point(10.0D, 10.0D);
     static final long TICK_LENGTH = 1000L;
     static final long RANDOM_SEED = 123L;
-    static final int NUM_VEHICLES = 200;
     static final int TEST_SPEEDUP = 30;
     static final long TEST_STOP_TIME = 60000000L;
 
     static final int DEPOTS = 4;
-    static final int UAVS = 4;
+    static final int UAVS = 1;
     static final int MAX_PARCELS = 100;
 
     static final boolean TESTING = true;
@@ -54,7 +51,9 @@ public final class SimGenerator {
     }
 
     public static void run(boolean testing) {
-        Builder viewBuilder = View.builder().with(PlaneRoadModelRenderer.builder())
+        Builder viewBuilder = View.builder()
+                //.with(PlaneRoadModelRenderer.builder())
+                .with(NoFlyZoneRoadModelRenderer.builder())
                 .with(RoadUserRenderer.builder()
                         .withImageAssociation(DistributionCenter.class, "/graphics/perspective/tall-building-64.png")
                         .withToStringLabel()
@@ -64,28 +63,34 @@ public final class SimGenerator {
             viewBuilder = viewBuilder.withSpeedUp(TEST_SPEEDUP).withAutoClose().withAutoPlay().withSimulatorEndTime(TEST_STOP_TIME);
         }
 
+        List<Rectangle> forbiddenZones = Arrays.asList(
+                new Rectangle(-4.0, 4.0, -4.0, 4.0),
+                new Rectangle(-8.0, 0.0, -2.0, 2.0));
         Simulator sim = Simulator.builder().setTickLength(1000L).setRandomSeed(RANDOM_SEED)
-                .addModel(RoadModelBuilders.plane().withMinPoint(MIN_POINT).withMaxPoint(MAX_POINT).withMaxSpeed(50.0D))
+                //.addModel(RoadModelBuilders.plane().withMinPoint(MIN_POINT).withMaxPoint(MAX_POINT).withMaxSpeed(50.0D))
+                .addModel(NoFlyZoneRMB.create().withMinPoint(MIN_POINT).withMaxPoint(MAX_POINT).withMaxSpeed(50.0D).withForbiddenZones(forbiddenZones))
                 .addModel(DefaultPDPModel.builder())
                 .addModel(CommModel.builder())
                 .addModel(viewBuilder)
                 .build();
 
         RoadModel rm = sim.getModelProvider().tryGetModel(RoadModel.class);
+        RandomGenerator rnd = sim.getRandomGenerator();
 
         ArrayList<DistributionCenter> depots = new ArrayList<>();
         // create and register the depots
         for (int i = 0; i < DEPOTS; i++) {
-            Point depotLocation = rm.getRandomPosition(sim.getRandomGenerator());
-            DistributionCenter depot = new DistributionCenter(depotLocation, 1337.0D, sim.getRandomGenerator());
+            Point depotLocation = rm.getRandomPosition(rnd);
+            DistributionCenter depot = new DistributionCenter(depotLocation, 1337.0D, rnd);
             sim.register(depot);
             depots.add(depot);
         }
 
         // create and register the UAVs
         for(int i = 0; i < UAVS; ++i) {
-            double speed = getRandomSpeed(sim.getRandomGenerator(), MAX_SPEED, MIN_SPEED);
-            UAV uav = new UAV(sim.getRandomGenerator(), speed, BAT_CAPACITY, MOT_POWER, MAX_SPEED);
+            double speed = getRandomSpeed(rnd, MAX_SPEED, MIN_SPEED);
+            Point startPos = rm.getRandomPosition(rnd);
+            UAV uav = new UAV(startPos, speed, BAT_CAPACITY, MOT_POWER, MAX_SPEED);
             sim.register(uav);
         }
 
@@ -94,15 +99,14 @@ public final class SimGenerator {
         sim.addTickListener(parcelgen);
 
 
-        System.out.println(sim.getModelProvider().getModel(PlaneRoadModel.class).getObjects().size());
+        // System.out.println(sim.getModelProvider().getModel(PlaneRoadModel.class).getObjects().size());
 
         sim.start();
     }
 
     public static double getRandomSpeed(RandomGenerator rnd, double minSpeed, double maxSpeed) {
         double random = rnd.nextDouble();
-        double speed = minSpeed + (maxSpeed - minSpeed) * random;
-        return speed;
+        return minSpeed + (maxSpeed - minSpeed) * random;
     }
 
 }
