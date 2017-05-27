634fade00fc7e92e8a7d90048ef8e8711645d316
Anthony Clays <anthony@clays.me>
2017-05-27 10:26:48 +0200

    Precomputation in DroneParcel
    Parcels now compute the shortest path between their location and
    destination, as well as between their destination and the nearest
    charging station.

--- src/main/java/agents/DroneParcel.java
+++ src/main/java/agents/DroneParcel.java
@@ -6,16 +6,22 @@ import com.github.rinde.rinsim.core.model.pdp.Parcel;
 import com.github.rinde.rinsim.core.model.road.RoadModel;
 import com.github.rinde.rinsim.geom.Point;
 
+import java.util.List;
+
 /**
  * Created by anthony on 5/11/17.
  */
 public class DroneParcel extends Parcel {
 
     private DistributionCenter depot;
+    private List<Point> shortestPath;
+    private List<Point> shortestEscape;
 
-    public DroneParcel(Point destination, DistributionCenter depot) {
+    public DroneParcel(Point destination, DistributionCenter depot, List<Point> shortestPath, List<Point> shortestEscape) {
         super(Parcel.builder(depot.getPosition().get(), destination).buildDTO());
         this.depot = depot;
+        this.shortestPath = shortestPath;
+        this.shortestEscape = shortestEscape;
     }
 
     @Override
@@ -25,5 +31,12 @@ public class DroneParcel extends Parcel {
 
     public DistributionCenter getDepot() {
         return depot;
+    }
+
+    public List<Point> getShortestPath() {
+        return shortestPath;
+    }
+    public List<Point> getShortestEscape() {
+        return shortestEscape;
     }
 }
